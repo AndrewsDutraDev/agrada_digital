38d9089a3e010f9f86d73c73b69478cabd1e016e
AndrewsDutraDev <dutrapgmdr@gmail.com>
2023-11-12 21:39:52 -0300

    Removed unnecessary files

--- src/js/components/home.js
+++
@@ -1,9 +0,0 @@
-
-
-const Home = {
-  init: function () {
-
-  },
-};
-
-export default Home;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 import Header from './components/header.js';
 import Products from './components/products.js';
-import Home  from  './components/home.js';
     
 Header.init();
 Products.init();
-// Home.init();
